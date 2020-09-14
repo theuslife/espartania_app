998069bade104fa474adb62782233754a0351d2d
matheus.silva <matheus.silva@buildings.com.br>
2020-09-14 20:55:17 -0300

    esp

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{color: 'white', fontSize: 40}}>Bem vindos</Text>
       <StatusBar style="auto" />
-    </View>
+    </View>                                       
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#a4031f',
     alignItems: 'center',
     justifyContent: 'center',
   },
